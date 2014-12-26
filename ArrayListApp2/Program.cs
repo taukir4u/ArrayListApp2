8dbf655aa5571454b814c84410d24fe5bb3c943a
taukir4u <taukir4u@gmail.com>
2014-12-26 19:05:30 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ ArrayListApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList newList = new ArrayList();
+
+            //Console.WriteLine("Size of ArrayList: " + newList.Count);
+
+            //Console.WriteLine("Trying to add values... ");
+
+            newList.Add(516);
+            newList.Add("Taukir");
+            newList.Add(176352.276278);
+
+            foreach (var values in newList)
+            //foreach (object values in newList)
+            {
+                Console.WriteLine(values);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
